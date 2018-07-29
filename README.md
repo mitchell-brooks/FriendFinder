d051807810fd0fd20f030899186f5917b75eaad9
Mitchell <MitchellBrooks@gmail.com>
2018-07-29 13:36:41 -0400

    Final commit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FriendFinder
+# FriendFinders
+This is a demo app for matching friends based on personality surveys. It utilizes Express and Node to present an interface for taking a survey and then logic for matching your answers to other survey respondents.
+
---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friendsArray = [
+  {
+    friendName: "Justin Vernon",
+    photo: "https://www.famousbirthdays.com/faces/vernon-justin-image.jpg",
+    scores:[
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+  },
+  {
+    friendName: "Justin Bland",
+    photo: "https://www.chshornets.com/photos/fbbland.jpg",
+    scores:[
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+  },
+  {
+    friendName: "Justin Timberlake",
+    photo: "https://www.grammy.com/sites/com/files/justintimberlake-spotlight-624536210.png",
+    scores:[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+  },
+  {
+    friendName: "Justin Roiland",
+    photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/c/ce/MortyTransparent.png/revision/latest?cb=20160909031949",
+    scores:[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  }
+];
+
+module.exports = friendsArray;
